b7ab18425469d114ee47166725e25deddb3d240f
Kimjae71 <nisroeld71@gmail.com>
2020-07-09 19:05:29 +0900

    winch

--- scripts/winch.py
+++ scripts/winch.py
@@ -1,12 +1,55 @@
 #!/usr/bin/env python
-import rospy
-from sunrise.msg import WayPoint
-from geometry_msgs.msg import Twist
+#import rospy
+#from sunrise.msg import WayPoint
+#from geometry_msgs.msg import Twist
 from math import sqrt
+from RPi import GPIO
+import time
+
+pwmPin = 19
+dirPin1 = 13
+dirPin2 = 6
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pwmPin, GPIO.OUT)
+GPIO.setup(dirPin1, GPIO.OUT)
+GPIO.setup(dirPin2, GPIO.OUT)
+clock = 0
+GPIO.output(dirPin1, 1)
+GPIO.output(dirPin2, 0)
+
+#encoder
+
+encPinA = 23
+encPinB = 24
+GPIO.setup(encPinA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(encPinB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+encoderPos = 0
+
+def encoderA(channel):
+    global encoderPos
+    GPIO.remove.event.detect(encPinA)
+    if GPIO.input(encPinA) == GPIO.input(encPinB):
+        encoderPos += 1
+    else:
+        encoderPos -= 1
+   
+def encoderB(channel):
+    global encoderPos
+    GPIO.remove.event.detect(encPinB)
+    if GPIO.input(encPinA) == GPIO.input(encPinB):
+        encoderPos -= 1
+    else:
+        encoderPos += 1
+
 
 class Winch:
     def __init__(self):
-        self.velLimit = 0
+        self.pwm = GPIO.PWM(pwmPin, 100)
+        self.pwm.start(100)
+        self.flag = 0
+        '''self.velLimit = 0
         self.angVelLimit = 0
         rospy.Subscriber('/sunrise/waypoint', WayPoint, self.waypoint)
         rospy.Subscriber('/mavros/global_position/gp_vel', Twist, self.velocity)
@@ -27,6 +70,18 @@ class Winch:
     def determine(self):
         if self.wp == 2 && self.vel < self.velLimit && self.angVel < self.angVelLimit :
             self.motor_run()
-
+'''
     def motor_run(self):
+        global encoderPos
+        for i in range(0,5000):
+            if i<4000:
+                self.pwmValue=100       
+            else:
+                self.pwmValue=0
+            GPIO.add_event_detect(encPinA, GPIO.BOTH, callback=encoderA, bouncetime=200)
+            GPIO.add_event_detect(encPinB, GPIO.BOTH, callback=encoderB, bouncetime=200)
+            print(encoderPos)
 
+if __name__=="__main__":
+    motor = Winch()
+    motor.motor_run()
---
+++ scripts/winch2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#import rospy
+#from sunrise.msg import WayPoint
+#from geometry_msgs.msg import Twist
+from math import sqrt
+from RPi import GPIO
+import time
+
+pwmPin = 19
+dirPin1 = 13
+dirPin2 = 6
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pwmPin, GPIO.OUT)
+GPIO.setup(dirPin1, GPIO.OUT)
+GPIO.setup(dirPin2, GPIO.OUT)
+clock = 0
+GPIO.output(dirPin1, 1)
+GPIO.output(dirPin2, 0)
+
+#encoder
+
+encPinA = 23
+encPinB = 24
+GPIO.setup(encPinA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(encPinB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+encoderPos = 0
+
+def encoderA(channel):
+    global encoderPos
+    global encPinA
+    global encPinB
+    GPIO.remove_event_detect(encPinA)
+    if GPIO.input(encPinA) == GPIO.input(encPinB):
+        encoderPos += 1
+    else:
+        encoderPos -= 1
+   
+def encoderB(channel):
+    global encoderPos
+    global encPinA
+    global encPinB
+    GPIO.remove_event_detect(encPinB)
+    if GPIO.input(encPinA) == GPIO.input(encPinB):
+        encoderPos -= 1
+    else:
+        encoderPos += 1
+
+
+if __name__=="__main__":
+    pwm = GPIO.PWM(pwmPin, 100)
+    pwm.start(100)
+    for i in range(0,50):
+        if i<40:
+            pwmValue=100       
+        else:
+            pwmValue=0
+        pwm.ChangeDutyCycle(pwmValue)
+        GPIO.add_event_detect(encPinA, GPIO.RISING, callback=encoderA)
+        GPIO.add_event_detect(encPinB, GPIO.RISING, callback=encoderB)
+        print(encoderPos)
+        time.sleep(0.2)
